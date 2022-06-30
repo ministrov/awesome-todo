aa4fecb293b8b4aa61d0336af690a9a2774320d8
ministrov <61511030+ministrov@users.noreply.github.com>
2022-06-30 11:26:35 +0300

    Did main header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <template>
   <div id="app">
+    <div class="content-wrapper">
+      <div class="main-header">
+        <h2 class="main-header__title">Todo List</h2>
 
+        <AddButton />
+      </div>
+
+
+    </div>
   </div>
 </template>
 
 <script>
-
+import AddButton from './components/AddButton.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    AddButton
+  }
 }
 </script>
 
-<style>
-/* #app {
-
-} */
+<style lang="scss">
+body {
+  max-width: 1940px;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 16px;
+  color: #ffffff;
+  background-color: #252E42;
+}
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.content-wrapper {
+  max-width: 420px;
+  height: 570px;
+  background-color: #4F5565;
+}
+.main-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 </style>
